83af05e0b95c8ec15400331dd2c0d61f29f44b62
Miki Tebeka <miki@353solutions.com>
2025-05-28 15:46:59 +0300

    marshal

--- value/value.go
+++ value/value.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"time"
@@ -20,6 +21,14 @@ object <-> struct, map[string]any
 MIA:
 - time.Time -> string in RFC3339 format
 - []byte -> string in base64
+
+encoding/json API
+JSON -> []byte -> Go: json.Unmarshal
+Go -> []byte -> JSON: json.Marshal
+JSON -> io.Reader -> Go: json.Decoder
+Go -> io.Writer -> JSON: json.Encoder
+
+You can use bytes.Buffer or bytes.Reader for in memory io.Reader/Writer
 */
 
 func main() {
@@ -73,4 +82,57 @@ func main() {
 	// wall clock: human
 	// monotonic: machine, going up only
 
+	v := Value{
+		Unit:   "cm",
+		Amount: 136.7,
+	}
+
+	var buf bytes.Buffer
+	json.NewEncoder(&buf).Encode(&v)
+	fmt.Println(buf.String())
 }
+
+// What json.Marshal does
+func Marshal(val any) ([]byte, error) {
+	if m, ok := val.(json.Marshaler); ok {
+		return m.MarshalJSON()
+	}
+
+	// do the defaults
+	return nil, nil
+
+}
+
+// Business: Show Value as JSON string in 136.7cm format
+
+/* In order for encoding/json to use MarshalJSON, the receiver type must match
+method: value, encoded: value: OK
+method: value, encode: pointer: OK
+method: pointer, encode: pointer: OK
+method: pointer, encode: value: NO
+*/
+
+// MarshalJSON implements json.Marshaler
+func (v Value) MarshalJSON() ([]byte, error) {
+	// Step 1: Convert to a type encoding/json can handle
+	s := fmt.Sprintf("%f%s", v.Amount, v.Unit)
+
+	// Step 2: Use json.Marshal
+	return json.Marshal(s)
+
+	// Step 3: There is no step 3
+}
+
+type Value struct {
+	Unit   string  `json:"unit,omitempty"` // field tag
+	Amount float64 `json:"amount,omitempty"`
+}
+
+/*
+API			stable: type User struct {}
+Business    fast  : type User struct {}
+Data		medium: type User struct {}
+
+DRY: Do not repeat yourself
+A little copying is better than a little dependency. (Go Team)
+*/
