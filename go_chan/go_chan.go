bf852a454f0703de5369080de8a9b0ccb4a229e6
Miki Tebeka <miki@353solutions.com>
2025-06-08 11:38:44 +0300

    taxi

--- go_chan/go_chan.go
+++ go_chan/go_chan.go
@@ -1,7 +1,21 @@
 package main
 
+/*
+Concurrency:
+- spin work
+- spin work, wait for finish
+	sync.WaitGroup
+- spin work, check error
+	- errgroup
+- spin work, get result
+	- channel
+*/
+
 import (
 	"fmt"
+	"log/slog"
+	"net/http"
+	"sync"
 	"time"
 )
 
@@ -50,6 +64,79 @@ func main() {
 	fmt.Printf("msg: %q\n", msg)
 	msg, ok := <-ch // ch is closed
 	fmt.Printf("msg: %q (ok=%v)\n", msg, ok)
+
+	urls := []string{
+		"https://go.dev",
+		"https://google.com",
+		"https://ibm.com/no/such/page",
+	}
+
+	// Only wait
+	start := time.Now()
+
+	var wg sync.WaitGroup
+	wg.Add(len(urls))
+	for _, url := range urls {
+		// wg.Add(1)
+		go func() {
+			defer wg.Done()
+			checkURL(url)
+		}()
+	}
+
+	wg.Wait()
+	duration := time.Since(start)
+	fmt.Printf("%d URLS in %v\n", len(urls), duration)
+
+	// Get result
+	start = time.Now()
+
+	ch1 := make(chan result)
+	for _, url := range urls {
+		go func() {
+			r := result{url: url}
+			r.status, r.err = urlInfo(url)
+			ch1 <- r
+		}()
+	}
+
+	for range urls {
+		r := <-ch1
+		fmt.Println(r)
+	}
+
+	duration = time.Since(start)
+	fmt.Printf("%d URLS in %v\n", len(urls), duration)
+
+}
+
+type result struct {
+	// call context
+	url string
+
+	status string
+	err    error
+}
+
+func urlInfo(url string) (string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+
+	defer resp.Body.Close()
+	return resp.Status, nil
+}
+
+func checkURL(url string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		slog.Error("checkURL", "url", url, "error", err)
+	}
+
+	defer resp.Body.Close()
+	slog.Info("checkURL", "url", url, "status", resp.Status)
+
 }
 
 /* Channel semantics
